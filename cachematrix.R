406efffe154c65ba059769081bf0e92e682037e5
Chadwick Bidwell <chadwickbidwell@Chadwicks-MacBook-Pro-2.local>
2018-11-06 15:53:29 -0800

    switching from solve to ginv. adding comments.

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,19 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix provides getters and setters for m and x 
+## which are free variables with global scope. Using global 
+## variables in this manner probably isn't good practice
+## due to the fact that other functions have access to the same
+## variables and can overwrite them leading to unpredictable 
+## behavior. 
+## cacheSolve checks if m is null. If it is then it calls ginv on
+## the matrix, otherwise it uses the previously saved global value.
+## To test, try the following:
+## foo = matrix(1:9, nrow=3, ncol=3)
+## boo = makeCacheMatrix(foo)
+## cacheSolve(boo)
+## execute cacheSolve(boo) again to confirm that is uses cache on
+## subsequent calls
 
-## Write a short comment describing this function
+## see above for comment
 
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
@@ -9,36 +21,23 @@ makeCacheMatrix <- function(x = matrix()) {
     x <<- y
     m <<- NULL
   }
-  
-  get <- function() {
-    x
-  }
-  
-  setsolve <- function(solve) {
-    m <<- solve
-  }
-  
-  getsolve <- function() {
-    m
-  }
-    
-  list(set = set, get = get, setsolve = setsolve, getsolve = getsolve) 
-
+  get <- function() { x }
+  setsolve <- function(solve) { m <<- solve }
+  getsolve <- function() { m }
 }
 
 
-## Write a short comment describing this function
+## see above for comment
 
 cacheSolve <- function(x, ...) {
+  require(MASS)
   m <- x$getsolve()
-  
   if (!is.null(m)) {
     message("getting cached data")
     return(m)
   }
-  
   data <- x$get()
-  m <- solve(data, ...)
+  m <- ginv(data, ...)
   x$setsolve(m)
   m
 }
